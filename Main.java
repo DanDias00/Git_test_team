a4192c12d1411e267c0b602fc5286aa5582b7f9b
Himesh-Kariyawasam <dulanjanamasaarachi12@gmail.com>
2021-05-13 12:22:11 +0530

    test by Himesh

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main (String[] args){
+        String name = "Himesh";
         System.out.println("Hello World");
 		System.out.println("Dan");
+
     }
 }
